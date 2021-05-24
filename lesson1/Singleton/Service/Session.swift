fae74175a4c47079bc48d1f0801f6cecc7c5ae38
Алексей Пеньков <alexey_penkov@list.ru>
2021-05-24 15:27:21 +0800

    Home work #1 Singleton

---
+++ lesson1/Singleton/Service/Session.swift
@@ -0,0 +1,18 @@
+//
+//  Session.swift
+//  lesson1
+//
+//  Created by Алексей Пеньков on 24.05.2021.
+//
+
+import Foundation
+
+class Session {
+    
+    private init() {}
+    
+    static let shared = Session()
+    
+    var token: String = ""
+    var userID: Int = 0
+}
